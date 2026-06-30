5163269e295d949c04ca5711fa9593b2f075946a
MarieJF <marie.jacquemin-fanti@student.hepl.be>
2026-06-30 09:50:57 +0200

    Feat : Make the navigation barre sticky

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
 window.addEventListener('load', () => {
     history.replaceState(null, '', window.location.pathname);
 });
+
+const header = document.querySelector('.header');
+const barre = document.querySelector('.navigation-barre-sticky');
+
+const observer = new IntersectionObserver(
+    ([entry]) => {
+        if (entry.isIntersecting) {
+            barre.classList.remove('is-fixed');
+        } else {
+            barre.classList.add('is-fixed');
+        }
+    }
+);
+
+observer.observe(header);
